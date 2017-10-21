9a83009785e363cbb66613756aeb10350f3fb363
Alfredo Quintero Tlacuilo <alfredoq98@hotmail.com>
2017-10-21 18:04:29 -0500

    Finally solved the god damn problem. The problem was that the muzzle location did not allow the projectile to spawn.

--- Source/TestingGrounds/Private/Gun.cpp
+++ Source/TestingGrounds/Private/Gun.cpp
@@ -43,7 +43,6 @@ void AGun::OnFire()
             const FRotator SpawnRotation = FP_MuzzleLocation->GetComponentRotation();
             // MuzzleOffset is in camera space, so transform it to world space before offsetting from the character location to find the final muzzle position
             const FVector SpawnLocation = FP_MuzzleLocation->GetComponentLocation();
-            
             //Set Spawn Collision Handling Override
             FActorSpawnParameters ActorSpawnParams;
             ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
